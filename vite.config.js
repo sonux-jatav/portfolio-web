b4dce943271a7d8c78cabf7676b09c0bb33a2e9c
Sonu Jatav <jatavsonu2k3@gmail.com>
2025-09-27 16:48:16 -0700

    fix: github pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio-web/",
-  plugins: [tailwindcss(),react()],
+  base: '/portfolio-web/', // yaha apne repo ka naam daalo
+  plugins: [react()],
 })
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react(),],
-// });
