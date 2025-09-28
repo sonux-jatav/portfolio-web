30919ae7cffb1db388128055cb60f36f7f4f702c
Sonu Jatav <jatavsonu2k3@gmail.com>
2025-09-27 17:18:49 -0700

    your message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-web/', // yaha apne repo ka naam daalo
-  plugins: [react()],
+  base: "/portfolio-web/",
+  plugins: [tailwindcss(),react()],
 })
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react(),],
+// });
