a79b8d358faf0513e3c02fe94709f9a89ff82c63
Sonu Jatav <jatavsonu2k3@gmail.com>
2025-09-30 16:51:30 -0700

    portfolio updated

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -1,22 +1,18 @@
-import { Link } from "react-scroll";
 import { LuArrowDownRight } from "react-icons/lu";
 
 const NavbarBtn = () => {
   return (
-    <button className="flex items-center gap-2 px-4 py-2 rounded-full text-lg sm:text-xl font-bold text-white border border-cyan bg-gradient-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange hover:shadow-cyanShadow cursor-pointer">
-      <Link
-        spy={true}
-        smooth={true}
-        duration={500}
-        offset={-120}
-        className="flex items-center gap-1"
-      >
-        Hire Me
-        <span className="hidden md:inline-block">
-          <LuArrowDownRight />
-        </span>
-      </Link>
-    </button>
+    <a
+      href="https://www.linkedin.com/in/sonujatav/" // apna LinkedIn link yaha daalna
+      target="_blank" // new tab me khulega
+      rel="noopener noreferrer" // security best practice
+      className="flex items-center gap-2 px-4 py-2 rounded-full text-lg sm:text-xl font-bold text-white border border-cyan bg-gradient-to-r from-darkCyan to-orange transition-all duration-500 hover:scale-110 hover:border-orange hover:shadow-cyanShadow cursor-pointer"
+    >
+      Hire Me
+      <span className="hidden md:inline-block">
+        <LuArrowDownRight />
+      </span>
+    </a>
   );
 };
 
